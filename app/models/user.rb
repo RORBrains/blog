195cf4673e41082aa65b06dc9c2ee314eeb4fce2
Ivan Zinovyev <vanyazin@gmail.com>
2018-03-22 21:51:44 +0300

    Associations between users/comments/posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 16, minimum: 2 }
   validates :name, uniqueness: true
+
+  has_many :posts # posts user_id
+  has_many :comments # comments user_id
+  has_many :commented_posts, through: :comments, source: :post
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Comment.destroy_all
+Post.destroy_all
+User.destroy_all
+
+def random_name
+  "#{FFaker::Internet.user_name}#{Time.new.to_i}"[0..16]
+end
+
+# Users
+hash_users = 10.times.map do
+  {
+    name:  random_name,
+    email: FFaker::Internet.safe_email
+  }
+end
+users = User.create!(hash_users)
+users.first(7).each { |u| u.update(creator: true) }
+users.first(2).each { |u| u.update(moderator: true) }
+
+# Posts
+creators = User.where(creator: true)
+hash_posts = 20.times.map do
+  {
+    title: FFaker::HipsterIpsum.paragraph,
+    body: FFaker::HipsterIpsum.paragraph,
+    user: creators.sample
+  }
+end
+posts = Post.create!(hash_posts)
+
+# Comments
+hash_comments = 200.times.map do
+  {
+    body: FFaker::HipsterIpsum.paragraph,
+    post: posts.sample,
+    user: creators.sample
+  }
+end
+Comment.create!(hash_comments)
+
+
+
+
