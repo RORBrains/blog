627940579f2abc84ee5405057d4b274b4e1b7918
Ivan Zinovyev <vanyazin@gmail.com>
2018-06-14 22:50:16 +0300

    Add devise secret key

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+
+#Rake::Task["deploy:assets:precompile"].clear_actions
+#Rake::Task["deploy:assets:backup_manifest"].clear_actions
